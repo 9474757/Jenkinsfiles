3865a32305874cfe86a9915e7d8784a4ad07dfa7
9474757 <9474757@gmail.com>
2019-09-16 17:50:35 +0300

    Create AWS-EKS-CREATE-CLUSTER-AND-NODES.sh

---
+++ AWS-EKS-CREATE-CLUSTER-AND-NODES.sh
@@ -0,0 +1,10 @@
+eksctl create cluster \
+--name BWRK-TEST-BY-MAN \
+--version 1.14 \
+--nodegroup-name standard-workers \
+--node-type t2.micro \
+--nodes 1 \
+--nodes-min 2 \
+--nodes-max 3 \
+--ssh-access --ssh-public-key=mak.pub \
+--node-ami auto
